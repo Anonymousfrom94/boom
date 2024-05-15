b1526e4c535b6b7f7063754124a76902ddb3fba1
osall <ousall535@gmail.com>
2024-05-15 11:22:58 +0200

    Consommable = objet

--- src/main/java/universite_paris8/iut/osall/boom/modele/item/Objet.java
+++
@@ -1,9 +0,0 @@
-package universite_paris8.iut.osall.boom.modele.item;
-
-import universite_paris8.iut.osall.boom.modele.item.Item;
-
-public class Objet extends Item {
-    public Objet(String nom) {
-        super(nom);
-    }
-}
