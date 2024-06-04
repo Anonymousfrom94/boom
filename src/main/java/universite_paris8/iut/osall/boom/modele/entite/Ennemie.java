4dcf5b1d9e42f60f0fdbc69c2466e091fd03f5d4
osall <ousall535@gmail.com>
2024-06-04 16:03:19 +0200

    ennemie n'apparait pas dans un mur

--- src/main/java/universite_paris8/iut/osall/boom/modele/entite/Ennemie.java
+++ src/main/java/universite_paris8/iut/osall/boom/modele/entite/Ennemie.java
@@ -4,8 +4,28 @@ import universite_paris8.iut.osall.boom.modele.Environnement.Environnement;
 
 public class Ennemie extends Acteur {
 
+    private int indice(int newX, int newY) {
+        int ligne, colonne;
+        colonne = newX / 16;
+        ligne = newY / 16;
+        return ligne * 30 + colonne;
+    }
+
+    private void random(){
+        int x = 0;
+        int y = 0;
+        do {
+            x = (int) (Math.random() * 470);
+            y = (int) (Math.random() * 470);
+        }
+        while(this.getEnvironnement().getMap().getTableau()[indice(x, y)] == 1);
+        this.setX(x);
+        this.setY(y);
+    }
+
     public Ennemie(Environnement environnement){
-        super(environnement, (int) (Math.random() * 470), (int) (Math.random() * 470), 4, 1);
+        super(environnement, 0, 0, 4, 1);
+        random();
     }
 }
 //(int) Math.random() * (480 - 1)git
